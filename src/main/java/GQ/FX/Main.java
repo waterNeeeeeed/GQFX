1402e36e6127313713a9769f7bc5ae9c2b695cf5
waterNeeeeeed <8gt8@163.com>
2017-02-12 00:45:37 +0800

    进一步加深对输出路径的理解

--- src/main/java/GQ/FX/Main.java
+++ src/main/java/GQ/FX/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../../FX/start/StartFrame.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("start/StartFrame.fxml"));
         if (null == fxmlLoader.getLocation()){
             throw new Exception("StartFrame.fxml not found");
         }
